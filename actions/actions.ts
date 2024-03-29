281dfce4cf947826cbbc040945670a163e4b5fda
Alan Dours <alandours93@gmail.com>
2024-03-29 20:08:48 -0300

    Add getContacts

---
+++ actions/actions.ts
@@ -0,0 +1,110 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+
+export const getContacts = async (search = "") => {
+  const filter = {
+    contains: search,
+    mode: "insensitive",
+  } as const
+
+  const contacts = await prisma.contact.findMany({
+    where: {
+      active: true,
+      OR: [
+        {
+          name: filter,
+        },
+        {
+          lastname: filter,
+        },
+        {
+          address: filter,
+        },
+        {
+          notes: filter,
+        },
+        {
+          Social: {
+            some: {
+              username: filter,
+            },
+          },
+        },
+        {
+          Social: {
+            some: {
+              platform: {
+                name: filter,
+              },
+            },
+          },
+        },
+        {
+          Number: {
+            some: {
+              number: filter,
+            },
+          },
+        },
+        {
+          Email: {
+            some: {
+              email: filter,
+            },
+          },
+        },
+        {
+          Alias: {
+            some: {
+              alias: filter,
+            },
+          },
+        },
+      ],
+    },
+    select: {
+      id: true,
+      name: true,
+      lastname: true,
+      photo: true,
+      birthday: true,
+      address: true,
+      notes: true,
+      yearMet: true,
+      favorite: true,
+      Social: {
+        select: {
+          id: true,
+          username: true,
+          label: true,
+          platform: true,
+        },
+      },
+      Number: {
+        select: {
+          id: true,
+          number: true,
+          type: true,
+          label: true,
+        },
+      },
+      Email: {
+        select: {
+          id: true,
+          email: true,
+          type: true,
+          label: true,
+        },
+      },
+      Alias: {
+        select: {
+          id: true,
+          alias: true,
+        },
+      },
+    },
+  })
+
+  return contacts
+}
