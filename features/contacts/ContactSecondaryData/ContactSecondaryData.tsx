6bf77a35e88630f2b09fb40c79e0f303a98a8567
Alan Dours <alandours93@gmail.com>
2024-07-22 02:35:42 -0300

    Show sections

---
+++ features/contacts/ContactSecondaryData/ContactSecondaryData.tsx
@@ -0,0 +1,91 @@
+import React, { useContext } from "react"
+import { Alias, NumberType } from "@prisma/client"
+
+import { Datafield } from "@/components/Datafield"
+import { Section } from "@/components/Section"
+import { CONTACT_SECTIONS } from "@/features/contacts/constants"
+import { ContactsContext } from "@/features/contacts/context"
+
+import { Notes, ContactSecondaryDataContainer } from "./styles"
+
+const getFieldData = (field, urlStart) => {
+  const {
+    number,
+    email,
+    username,
+    platform,
+    type,
+    label: customLabel,
+  } = field || {}
+
+  const { prefix, url: platformUrl, name: platformName } = platform || {}
+
+  const name = number || email || `${prefix || ""}${username}`
+
+  const label =
+    customLabel && (type === NumberType.Custom || type === NumberType.Custom)
+      ? customLabel
+      : type || platformName
+
+  let url = urlStart
+
+  if (platform) {
+    url = `https://${platformUrl || ""}/${username}`
+  } else {
+    url += number || email
+  }
+
+  return { name, label, url }
+}
+
+const renderAliases = (aliases: Alias[]) =>
+  aliases.map((alias) => {
+    const { id, alias: aliasName } = alias || {}
+
+    return <Datafield name={aliasName} key={id + alias} />
+  })
+
+const renderMultiFields = (data: Record<string, unknown>[], urlStart: string) =>
+  data.map((field) => {
+    const { id } = field || {}
+    const { name, label, url } = getFieldData(field, urlStart) || {}
+    return <Datafield name={name} label={label} url={url} key={id + name} />
+  })
+
+const renderSection = (
+  key: (typeof CONTACT_SECTIONS)[number]["key"],
+  data: Record<string, unknown>[] | string,
+  urlStart: string
+) => {
+  switch (key) {
+    case "notes":
+      return <Notes>{data as string}</Notes>
+    case "Alias":
+      return renderAliases(data as Alias[])
+    default:
+      return renderMultiFields(data as Record<string, unknown>[], urlStart)
+  }
+}
+
+export const ContactSecondaryData = () => {
+  const { selectedContact } = useContext(ContactsContext)
+
+  return (
+    selectedContact && (
+      <ContactSecondaryDataContainer>
+        {CONTACT_SECTIONS.map((section) => {
+          const { title, icon, key, order, urlStart } = section
+          const data = selectedContact[key]
+
+          return (
+            !!data?.length && (
+              <Section icon={icon} key={key} order={order} sticky title={title}>
+                {renderSection(key, data, urlStart)}
+              </Section>
+            )
+          )
+        })}
+      </ContactSecondaryDataContainer>
+    )
+  )
+}
--- features/contacts/ContactSecondaryData/styles.ts
+++ features/contacts/ContactSecondaryData/styles.ts
@@ -8,6 +8,6 @@ export const ContactSecondaryDataContainer = styled.div`
 export const Notes = styled.p`
   color: ${({ theme }) => theme.selected.contrast[1]};
   margin: 1rem 0;
-  padding: 0.25rem 1.75rem;
+  padding: 0.5rem 0.75rem;
   white-space: pre-wrap;
 `
