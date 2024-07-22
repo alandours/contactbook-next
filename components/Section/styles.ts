6c625c38aabbf9ccfb23569bc55ddd808cf57c4c
Alan Dours <alandours93@gmail.com>
2024-07-22 19:38:18 -0300

    Fix sticky sections

--- components/Section/styles.ts
+++ components/Section/styles.ts
@@ -12,8 +12,8 @@ export const SectionContainer = styled.section<{ order: number }>`
 `
 
 export const Content = styled.div`
-  margin: 1rem 1.5rem;
   display: flex;
   flex-direction: column;
   overflow-x: scroll;
+  padding: 1rem 1.5rem;
 `
