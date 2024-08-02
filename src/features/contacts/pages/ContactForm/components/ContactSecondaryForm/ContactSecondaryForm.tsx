90f5f22af49152b3bef956bcea6b790645e0842e
Alan Dours <alandours93@gmail.com>
2024-08-02 04:27:09 -0300

    Add tags section to form

--- src/features/contacts/pages/ContactForm/components/ContactSecondaryForm/ContactSecondaryForm.tsx
+++ src/features/contacts/pages/ContactForm/components/ContactSecondaryForm/ContactSecondaryForm.tsx
@@ -6,6 +6,7 @@ import { AliasesSection } from '../Sections/AliasesSection'
 import { NumbersSection } from '../Sections/NumbersSection'
 import { EmailsSection } from '../Sections/EmailsSection'
 import { SocialsSection } from '../Sections/SocialsSection'
+import { TagsSection } from '../Sections/TagsSection'
 
 export const ContactSecondaryForm = () => (
   <>
@@ -13,6 +14,7 @@ export const ContactSecondaryForm = () => (
     <NumbersSection />
     <EmailsSection />
     <SocialsSection />
+    <TagsSection />
     <Section title="Notes" icon={Icons.notes} sticky>
       <Textarea name="notes" />
     </Section>
